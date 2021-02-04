6859abe9d628735d1df5e3ed7b76d5bff0341e0d
toneyparky <qkrtmddhks95@gmail.com>
2021-02-04 15:32:00 +0900

    feat: Maintenance 엔티티 구현

---
+++ src/main/java/com/openhack/toyland/Maintenance.java
@@ -0,0 +1,29 @@
+package com.openhack.toyland;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Maintenance extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Long toyId;
+
+    private Boolean healthCheck;
+
+    private LocalDateTime active;
+}
