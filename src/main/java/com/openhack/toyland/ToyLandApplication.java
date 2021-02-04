8e085c625c55f8264cabb0951fd6c85cb01b4a4e
toneyparky <qkrtmddhks95@gmail.com>
2021-02-04 15:25:13 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/openhack/toyland/ToyLandApplication.java
@@ -0,0 +1,13 @@
+package com.openhack.toyland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyLandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyLandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openhack/toyland/ToyLandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openhack.toyland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyLandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
