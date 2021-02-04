c2f8f13a01267765311191f42f62ee963aac1bd0
soyeon Park <sypark9646@gmail.com>
2021-02-05 08:01:16 +0900

    Feat/Infinite-scroll-pagination (#12)
    * feat: 기본 findAll에 pageable 적용
    
    * feat: filtering 구현

--- src/main/java/com/openhack/toyland/controller/ToyController.java
+++ src/main/java/com/openhack/toyland/controller/ToyController.java
@@ -1,10 +1,18 @@
 package com.openhack.toyland.controller;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
-
-import javax.validation.Valid;
-
+import com.openhack.toyland.dto.ToyCreateRequest;
+import com.openhack.toyland.dto.ToyDetailResponse;
+import com.openhack.toyland.dto.ToyResponse;
+import com.openhack.toyland.service.toy.ToyCreateService;
+import com.openhack.toyland.service.toy.ToyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,41 +22,46 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.openhack.toyland.dto.ToyCreateRequest;
-import com.openhack.toyland.dto.ToyDetailResponse;
-import com.openhack.toyland.dto.ToyResponse;
-import com.openhack.toyland.service.toy.ToyCreateService;
-import com.openhack.toyland.service.toy.ToyService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @CrossOrigin("*")
 @RequiredArgsConstructor
 @RequestMapping("/api/toys")
 @RestController
 public class ToyController {
-    private final ToyService service;
-    private final ToyCreateService createService;
+	private final ToyService service;
+	private final ToyCreateService createService;
 
-    @PostMapping
-    public ResponseEntity<Void> create(@RequestBody @Valid ToyCreateRequest request) {
-        Long id = createService.create(request);
-        log.info(Long.toString(id));
-        return ResponseEntity.created(URI.create("/api/toys/" + id)).build();
-    }
+	@PostMapping
+	public ResponseEntity<Void> create(@RequestBody @Valid ToyCreateRequest request) {
+		Long id = createService.create(request);
+		log.info(Long.toString(id));
+		return ResponseEntity.created(URI.create("/api/toys/" + id)).build();
+	}
 
-    @GetMapping
-    public ResponseEntity<List<ToyResponse>> findAll() {
-        List<ToyResponse> responses = service.findAll();
-        log.info(responses.toString());
-        return ResponseEntity.ok(responses);
-    }
+	@GetMapping
+	public ResponseEntity<List<ToyResponse>> findAll(
+		@PageableDefault(size = 6, sort = "created_date", direction = Sort.Direction.DESC) Pageable pageable,
+		@RequestParam(required = false) List<Long> organization,
+		@RequestParam(required = false) List<Long> skill,
+		@RequestParam(required = false) List<String> category,
+		@RequestParam(required = false) List<String> period,
+		@RequestParam(required = false) List<String> search
+	) {
+		log.info("[get toy condition] - " + pageable.toString());
+		log.info("[get toy condition] - " + organization);
+		log.info("[get toy condition] - " + skill);
+		log.info("[get toy condition] - " + category);
+		log.info("[get toy condition] - " + period);
+		log.info("[get toy condition] - " + search);
+		List<ToyResponse> responses = service.findAll(pageable, organization, skill, category, period, search);
+		log.info(responses.toString());
+		return ResponseEntity.ok(responses);
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ToyDetailResponse> findById(@PathVariable Long id) {
-        ToyDetailResponse response = service.findById(id);
-        log.info(response.toString());
-        return ResponseEntity.ok(response);
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<ToyDetailResponse> findById(@PathVariable Long id) {
+		ToyDetailResponse response = service.findById(id);
+		log.info(response.toString());
+		return ResponseEntity.ok(response);
+	}
 }
--- src/main/java/com/openhack/toyland/service/toy/ToyService.java
+++ src/main/java/com/openhack/toyland/service/toy/ToyService.java
@@ -2,30 +2,33 @@ package com.openhack.toyland.service.toy;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
-import org.springframework.stereotype.Service;
-
 import com.openhack.toyland.domain.toy.Toy;
 import com.openhack.toyland.domain.toy.ToyRepository;
+import com.openhack.toyland.domain.toy.ToySearchAndSortRepository;
 import com.openhack.toyland.dto.ToyDetailResponse;
 import com.openhack.toyland.dto.ToyResponse;
 import com.openhack.toyland.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class ToyService {
-    private final ToyRepository repository;
+	private final ToyRepository toyRepository;
+	private final ToySearchAndSortRepository toySearchAndSortRepository;
 
-    public List<ToyResponse> findAll() {
-        return repository.findAll().stream()
-            .map(ToyResponse::new)
-            .collect(Collectors.toList());
-    }
+	public List<ToyResponse> findAll(Pageable pageable, List<Long> organization, List<Long> skill,
+		List<String> category, List<String> period, List<String> search) {
+		return toySearchAndSortRepository.findToysByConditions(pageable, organization, skill, category, period, search)
+			.stream()
+			.map(ToyResponse::new)
+			.collect(Collectors.toList());
+	}
 
-    public ToyDetailResponse findById(Long id) {
-        Toy toy = repository.findById(id)
-            .orElseThrow(() -> new EntityNotFoundException("해당되는 toy가 없습니다."));
-        return new ToyDetailResponse(toy);
-    }
+	public ToyDetailResponse findById(Long id) {
+		Toy toy = toyRepository.findById(id)
+			.orElseThrow(() -> new EntityNotFoundException("해당되는 toy가 없습니다."));
+		return new ToyDetailResponse(toy);
+	}
 }
