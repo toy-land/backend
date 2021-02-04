8aaff3e89553b68643ea28fe00be5de4df5912cd
toneyparky <qkrtmddhks95@gmail.com>
2021-02-04 15:29:34 +0900

    feat: Toy 엔티티 구현
    - Category, Period 구현
    - BaseTimeEntity 구현

---
+++ src/main/java/com/openhack/toyland/toy/Period.java
@@ -0,0 +1,13 @@
+package com.openhack.toyland.toy;
+
+public enum Period {
+    LESS_THAN_A_DAY,
+    LESS_THAN_THREE_DAYS,
+    LESS_THAN_A_WEEK,
+    LESS_THAN_TWO_WEEKS,
+    LESS_THAN_A_MONTH,
+    LESS_THAN_THREE_MONTHS,
+    LESS_THAN_SIX_MONTHS,
+    LESS_THAN_A_YEAR,
+    MORE_THAN_A_YEAR
+}
---
+++ src/main/java/com/openhack/toyland/toy/Toy.java
@@ -0,0 +1,48 @@
+package com.openhack.toyland.toy;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.validation.constraints.NotNull;
+
+import com.openhack.toyland.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Toy extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    @Lob
+    private String description;
+
+    @Lob
+    private String logoUrl;
+
+    @Lob
+    private String link;
+
+    private Integer organizationId;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Enumerated(EnumType.STRING)
+    private Period period;
+}
