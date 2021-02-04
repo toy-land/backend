0a4d6fc862c4ad8f2c58c3a4049b0ce25b5c2d1e
soyeon <sypark9646@gmail.com>
2021-02-04 18:48:51 +0900

    feat: organization, skill 초기화 데이터 추가

---
+++ src/main/resources/init.sql
@@ -0,0 +1,30 @@
+use toy_land_dev;
+insert into organization(id, name, created_date, updated_date)
+values(1, 'YAPP', now(), now()),
+      (2, 'SOPT', now(), now()),
+      (3, 'DDD', now(), now()),
+      (4, '디프만', now(), now()),
+      (5, 'Mash-up', now(), now()),
+      (6, 'Fun.d', now(), now()),
+      (7, '프로그라피', now(), now()),
+      (8, 'NEXTERS', now(), now()),
+      (9, '피로그래밍', now(), now()),
+      (10, 'DnD', now(), now()),
+      (11, '네이버 핵데이', now(), now()),
+      (12, '우아한 테크캠프', now(), now());
+
+insert into skill(id, name, created_date, updated_date)
+values(1, 'java', now(), now()),
+      (2, 'javascript', now(), now()),
+      (3, 'spring', now(), now()),
+      (4, 'spring boot', now(), now()),
+      (5, 'react', now(), now()),
+      (6, 'vue.js', now(), now()),
+      (7, 'android', now(), now()),
+      (8, 'kotlin', now(), now()),
+      (9, 'swift', now(), now()),
+      (10, 'ios', now(), now()),
+      (11, 'typescript', now(), now()),
+      (12, 'python', now(), now()),
+      (13, 'django', now(), now());
+
