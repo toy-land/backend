105d7bbc293b4240abadcb1f5799b670369d383d
soyeon <sypark9646@gmail.com>
2021-02-05 22:51:01 +0900

    style: domain 수정

--- src/main/java/com/openhack/toyland/dto/EmailRequestBody.java
+++ src/main/java/com/openhack/toyland/dto/EmailRequestBody.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class EmailRequestBody {
 
-    private String senderAddress = "no_reply@toy-land.com";
+    private String senderAddress = "no_reply@toy-land.co.kr";
     private String title = "[토이랜드] 프로젝트 사용 불가능 기간 알림 서비스";
     private String body = "당신의 토이 프로젝트가 ${SLEEP_DAYS}일 동안 잠자고 있어요~~ㅠㅠ";
     private List<EmailParticipant> recipients;
