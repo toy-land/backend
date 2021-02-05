a8f401400e9a623fa43a512728a9054e2b448c88
toneyparky <qkrtmddhks95@gmail.com>
2021-02-05 18:18:46 +0900

    feat: auto-complete api 구현

--- src/main/java/com/openhack/toyland/controller/ToyController.java
+++ src/main/java/com/openhack/toyland/controller/ToyController.java
@@ -1,19 +1,10 @@
 package com.openhack.toyland.controller;
 
-import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
-import com.openhack.toyland.dto.DeleteToyRequstBody;
-import com.openhack.toyland.dto.SimpleSuccessResponse;
-import com.openhack.toyland.dto.ToyCreateRequest;
-import com.openhack.toyland.dto.ToyDetailResponse;
-import com.openhack.toyland.dto.ToyResponse;
-import com.openhack.toyland.dto.UpdateToyRequestBody;
-import com.openhack.toyland.service.toy.ToyCreateService;
-import com.openhack.toyland.service.toy.ToyService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
+
+import javax.validation.Valid;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -28,6 +19,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.openhack.toyland.dto.DeleteToyRequstBody;
+import com.openhack.toyland.dto.SimpleSuccessResponse;
+import com.openhack.toyland.dto.SimpleToyResponse;
+import com.openhack.toyland.dto.ToyCreateRequest;
+import com.openhack.toyland.dto.ToyDetailResponse;
+import com.openhack.toyland.dto.ToyResponse;
+import com.openhack.toyland.dto.UpdateToyRequestBody;
+import com.openhack.toyland.service.toy.ToyCreateService;
+import com.openhack.toyland.service.toy.ToyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @CrossOrigin("*")
@@ -70,6 +73,13 @@ public class ToyController {
         ToyDetailResponse response = service.findById(id);
         log.info(response.toString());
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/auto-complete")
+    public ResponseEntity<List<SimpleToyResponse>> autoComplete(@RequestParam(required = false) List<String> search) {
+        log.info("[get toy condition] - " + search);
+        List<SimpleToyResponse> responses = service.search(search);
+        return ResponseEntity.ok(responses);
     }
 
     @PutMapping("/{id}")
