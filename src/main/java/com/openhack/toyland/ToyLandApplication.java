14ad3afb057d7c0536ab6b0b015cf8207d1be32c
toneyparky <qkrtmddhks95@gmail.com>
2021-02-05 15:09:01 +0900

    refactor: RestTemplateConfig 추가

--- src/main/java/com/openhack/toyland/ToyLandApplication.java
+++ src/main/java/com/openhack/toyland/ToyLandApplication.java
@@ -2,19 +2,13 @@ package com.openhack.toyland;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ToyLandApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ToyLandApplication.class, args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
     }
 }
