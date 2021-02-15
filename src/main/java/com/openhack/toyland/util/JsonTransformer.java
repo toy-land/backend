65e6646f95f98d950522544fb7133b1da7564d16
toneyparky <qkrtmddhks95@gmail.com>
2021-02-16 08:45:51 +0900

    refactor: 불필요한 빈 등록 제거

--- src/main/java/com/openhack/toyland/util/JsonTransformer.java
+++ src/main/java/com/openhack/toyland/util/JsonTransformer.java
@@ -1,13 +1,10 @@
 package com.openhack.toyland.util;
 
-import org.springframework.stereotype.Component;
-
 import com.google.gson.Gson;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-@Component
 public class JsonTransformer {
     private static final Gson gson = new Gson();
 
