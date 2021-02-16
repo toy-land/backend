222028c4da550ea15189aa33ce538197bc6ae2fa
soyeon <sypark9646@gmail.com>
2021-02-16 13:27:32 +0900

    test: 의존성 및 properties 추가

--- src/test/java/com/openhack/toyland/service/maintenance/ScheduleServiceTest.java
+++ src/test/java/com/openhack/toyland/service/maintenance/ScheduleServiceTest.java
@@ -7,9 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource(properties = "app.scheduling.enable=false")
 @SpringBootTest
 public class ScheduleServiceTest {
 
