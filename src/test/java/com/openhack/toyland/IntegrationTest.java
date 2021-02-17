30c906a805ea1f05ef987b6a8e201eb270bc1067
soyeon <sypark9646@gmail.com>
2021-02-18 04:47:42 +0900

    test: init database

--- src/test/java/com/openhack/toyland/IntegrationTest.java
+++ src/test/java/com/openhack/toyland/IntegrationTest.java
@@ -1,21 +1,31 @@
 package com.openhack.toyland;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.support.TestPropertySourceUtils;
 import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@Sql(scripts = {"/db/init-test.sql", "/db/insert-test.sql"})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ContextConfiguration(initializers = IntegrationTest.Initializer.class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Testcontainers
 public abstract class IntegrationTest {
 
@@ -38,5 +48,11 @@ public abstract class IntegrationTest {
                 "spring.datasource.password=" + mariaDBContainer.getPassword()
             );
         }
+    }
+
+    @BeforeEach
+    public void setUp() {
+        Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(log);
+        mariaDBContainer.followOutput(logConsumer);
     }
 }
