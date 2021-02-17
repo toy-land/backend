197cee6214b6f88edf1ae21cffbd97bb3c094f46
soyeon <sypark9646@gmail.com>
2021-02-18 02:08:10 +0900

    test: restTemplateConfig autowired

--- src/test/java/com/openhack/toyland/service/maintenance/MaintenanceServiceTest.java
+++ src/test/java/com/openhack/toyland/service/maintenance/MaintenanceServiceTest.java
@@ -12,8 +12,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.openhack.toyland.config.RestTemplateConfig;
 
-@SpringBootTest
+@SpringBootTest(classes = {RestTemplateConfig.class})
 class MaintenanceServiceTest {
     @Autowired
     private RestTemplate restTemplate;
