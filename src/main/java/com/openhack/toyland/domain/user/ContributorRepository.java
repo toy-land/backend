6c17449519f6148fd5f61e46a8610702c4a67239
soyeon <sypark9646@gmail.com>
2021-04-12 16:34:55 +0900

    fix: find techstack & contributors with toyId

--- src/main/java/com/openhack/toyland/domain/user/ContributorRepository.java
+++ src/main/java/com/openhack/toyland/domain/user/ContributorRepository.java
@@ -1,7 +1,10 @@
 package com.openhack.toyland.domain.user;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContributorRepository extends JpaRepository<Contributor, Long> {
     void deleteAllByToyId(Long toyId);
+
+    List<Contributor> findAllByToyId(Long toyId);
 }
